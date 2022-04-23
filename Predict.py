55c7a52903776983f2445ee88dfa78ea8ab20497
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-04-23 11:07:15 +0100

    Displayed prediction on canvas after drawing

--- Predict.py
+++ Predict.py
@@ -3,18 +3,17 @@ from tensorflow.keras.models import load_model
 import pygame
 import numpy as np
 
+
 pygame.init()
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
-
+FONT = pygame.font.SysFont("arial", 25)
 
 MODEL = load_model("mnist.model")
 
-'''
 PREDICTIONS = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
                6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-'''
 
 
 pygame.init()
@@ -26,8 +25,15 @@ pygame.display.update()
 pygame.display.set_caption("Digit recogniser")
 
 
+
+def displayPrediction(nnGuess):
+    s = FONT.render("Guess: " + str(nnGuess), True, "red")
+    screen.blit(s, [3, 3])
+
+
 running = True
 writing = False
+
 
 while running:
 
@@ -39,7 +45,7 @@ while running:
 
         if event.type == pygame.MOUSEMOTION and writing:
             x, y = event.pos
-            pygame.draw.circle(screen, "black", (x, y), 17)
+            pygame.draw.rect(screen, "black", [x, y, 30, 30])
 
 
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -55,8 +61,10 @@ while running:
             img = cv.imread("img.png")[:, :, 0]
             img = cv.resize(img, (28, 28))
             img = np.invert(np.array([img]))
+
             prediction = MODEL.predict(img)
             print(np.argmax(prediction))
+            displayPrediction(PREDICTIONS.get(int(np.argmax(prediction))))
 
 
     pygame.display.update()
